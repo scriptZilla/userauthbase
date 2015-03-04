801abe06663de3c5252917256cd025996d5e745f
scriptZilla <david.lowery@colorado.edu>
2015-03-03 18:11:41 -0700

    Tweak views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :items
+
+  resources :roles
+
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
